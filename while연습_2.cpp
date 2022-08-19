e6353d7c991cf21f2a48b9505bf39e2f5970d9aa
woong <103757515+woong12@users.noreply.github.com>
2022-08-19 14:09:01 +0900

    Add files via upload
    08.19 while문 수업

---
+++ while연습_2.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+main(){
+	//	int i = 1, n, sum = 0;
+//	
+//	printf("합을 구할 마지막 수 입력 : ") ;
+//	scanf("%d", &n);
+//	
+//	while(i <= n){
+//		sum += i;
+//		i++;
+//	}
+//	printf("1부터 %d까지의 합은 %d입니다\n", n, sum);
+
+
+
+
+
+
+//	int i, n , sum = 0;
+//	int start;
+//	
+//	printf("시작 수 : ");
+//	scanf("%d", &i);
+//	printf("마지막 수 : ");
+//	scanf("%d", &n);
+//	start = i;
+//	while(i <= n){
+//		sum = sum +i;
+//		i++;
+//	}
+//	
+//	printf("%d부터 %d까지의 합은 %d", start, n,sum );
+//	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ whlie연습_4.cpp
@@ -0,0 +1,97 @@
+#include<stdio.h>
+
+main(){
+	//	int count = 1;
+//	
+//	while (1) {
+//		printf("%d ", count);
+//		count++;
+//		
+//		if(count > 10) 
+//		break;
+//	}
+//
+
+//	int i = 1, sum = 0;
+//	while (true) {
+//		sum += i;
+//		i += 2;
+//		if(i > 100)
+//		break;
+//	}
+//	printf("1부터 100까지의 홀수의 합 = %d\n", sum);
+//	
+
+
+	int i = 0, sum = 0;
+	while(1){
+		sum += i;
+		i += 3;
+		
+		if(i > 100)
+		break;
+		printf("%d\n", i);
+	}
+	printf("1부터 100까지의 3의 배수의 합 = %d\n", sum);
+
+
+	
+//	int i = 1;
+//	
+//	do{
+//		printf("%d ", 11-i);
+//		i++;
+//	}while(i <= 6);
+
+
+//	int n, i = 0;
+//	printf("정수입력 : ");
+//	scanf("%d", &n);
+//	
+//	do{
+//		printf("I love programing\n");
+//		i++;
+//	}while(i < n);
+
+
+	int num;
+	
+	do{
+		scanf("%d", &num);
+		
+		if (num > 0){
+			if{
+			num % 2 == 1;
+			printf("홀수\n");
+		 else 
+			printf("짝수\n");
+		}
+		
+	
+	}while(num >0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
