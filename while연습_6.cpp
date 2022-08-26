1330f1e91a39d33707ef1e2c08313c30f1ea7f23
woong <103757515+woong12@users.noreply.github.com>
2022-08-26 14:08:59 +0900

    Add files via upload

---
+++ while연습_6.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+	
+
+	
+	
+	
+	
+	
+
+ 
+void func1(){
+	printf("1번 함수입니다.\n");
+	
+		int i = 0, sum = 0;
+	
+	while(1){
+		if(i > 10)
+			break;
+		if(i % 5 == 0){
+			continue;
+		sum+=1;			
+		}
+
+	}
+	printf("sum = %d\n", sum);
+}
+
+void func2(){
+	printf("2번 함수입니다.\n");
+	
+		int n, count = 0, sum = 0;
+	do{
+		scanf("%d", &n);
+		if(n < 1){
+			continue;
+		}
+		sum += n;
+		count ++;
+	}while(count <=4);
+	printf("합계 : %d\n", sum);
+}
+
+
+
+
+main(){
+	
+	int no;
+	while (1){
+		printf("함수번호 : ");
+		scanf("%d", &no);
+		
+		switch(no){
+			case 1:
+				func1();
+				break;
+			case 2:
+				func2();
+				break;
+			case 0:
+				return 0;
+			default:
+				printf("잘못된 함수 번호입니다.");
+		}
+	}
+}
+
+
+
+
+
+
