113b0f1cf77f01ba04e6e0ba9554313e0ef50b24
woong <103757515+woong12@users.noreply.github.com>
2022-09-30 10:48:43 +0900

    Add files via upload

---
+++ while연습_3.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+main(){
+	 //	int i = 1;
+//	printf("*** 2단 ***\n");
+//	
+//	while(i < 10){
+//		printf("2 * %d = %d\n", i, 2*i);
+//		i++;
+//	}
+
+
+//	int dan, i = 9; 
+//	printf("몇 단 ? ");
+//	scanf("%d", &dan);
+//	
+//	while(i > 0){
+//		printf("%d * %d = %d\n", dan, i, dan*i);
+//		i--;
+//	}
+
+//int i = 1, dan;
+//
+//while(1){
+//	i = 1;
+//	
+//	printf("몇 단?[0 : 종료]");
+//	scanf("%d", &dan);
+//	
+//		if(dan == 0){
+//			break;
+//		}
+//		
+//	printf("\n*** %d단 ***\n", dan);
+//	
+//	while(i <= 9){
+//		printf("%d * %d = %d\n", dan, i, dan*i);
+//		i++;
+//		
+//	
+//	}
+//	
+//}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+}
